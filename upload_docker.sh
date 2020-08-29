d7d6c23ee3fdebba55dbc96d83d01b613462ec7d
mmbayoumi86 <61152575+mmbayoumi86@users.noreply.github.com>
2020-08-29 23:54:42 +0300

    Devops

---
+++ upload_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath=minorpatch/udacity-cloud-devops-p4
+
+# Step 2:
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login -u minorpatch
+docker tag api $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
